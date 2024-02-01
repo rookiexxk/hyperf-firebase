b712372705d9b150225aeb3703cd8ebda2d0418b
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2024-02-01 13:56:19 +0800

    Remove unnecessary comments and update links in configuration files

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -2,19 +2,24 @@
 
 declare(strict_types=1);
 /**
- * This file is part of sms.
+ * This file is part of hyperf-firebase.
  *
- * @link     https://code.addcn.com/tw591ms/sms
- * @document https://code.addcn.com/tw591ms/sms
- * @contact  hdj@addcn.com
+ * @link     https://github.com/fcorz/hyperf-firebase
+ * @document https://github.com/fcorz/hyperf-firebase/blob/main/README.md
+ * @contact  fengchenorz@gmail.com
+ * @license  https://github.com/fcorz/hyperf-firebase/blob/main/LICENSE
  */
+
 namespace PHPSTORM_META;
 
-    // Reflect
-    override(\Psr\Container\ContainerInterface::get(0), map(['' => '@']));
-    override(\Hyperf\Context\Context::get(0), map(['' => '@']));
-    override(\make(0), map(['' => '@']));
-    override(\di(0), map(['' => '@']));
-    override(\app(0), map(['' => '@']));
-    override(\optional(0), type(0));
-    override(\tap(0), type(0));
+use Hyperf\Context\Context;
+use Psr\Container\ContainerInterface;
+
+// Reflect
+override(ContainerInterface::get(0), map(['' => '@']));
+override(Context::get(0), map(['' => '@']));
+override(\make(0), map(['' => '@']));
+override(\di(0), map(['' => '@']));
+override(\app(0), map(['' => '@']));
+override(\optional(0), type(0));
+override(\tap(0), type(0));
